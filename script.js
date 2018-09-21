f90d18af1a1810bd48664688147078499a6d1001
Renan Rodrigo <renanrb@ime.usp.br>
2018-09-21 11:59:18 -0300

    Adicionar ex. condicional, entrada, saida e tratamento

---
+++ script.js
@@ -0,0 +1,26 @@
+// let nome = prompt("Digite aqui seu nome");
+
+// console.log(`O nome digitado foi ${nome}`);
+
+// alert(`Olá! Seja bem vindo, ${nome}!!!`);
+
+let entrada = prompt("Digite um número:");
+
+if(entrada.trim()){
+  let numero = Number(entrada);
+  if(numero > 15){
+    console.log("Número maior que quinze.");
+  }
+  else if(numero < 15){
+    console.log("Número menor que quinze.");
+  }
+  else if(numero === 15){
+    console.log("Número igual a quinze.");
+  }
+  else{
+    console.log("Valor inválido.");
+  }
+}
+else{
+  console.log("Nenhum valor digitado.");
+}
